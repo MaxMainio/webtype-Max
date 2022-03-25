9a5f3e61f5b25f0bb67561241dca5f211c03aac8
MaxMainio <beidler.max@gmail.com>
2022-03-24 20:29:11 -0400

    Even More Perfect!

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,7 @@
 console.log("https://www.youtube.com/watch?v=NuAKnbIr6TE");
+
+$(document).ready(function(){
+    $("#dark-mode-toggle").click(function(){
+        $("body").toggleClass("darkMode");
+    });
+});
