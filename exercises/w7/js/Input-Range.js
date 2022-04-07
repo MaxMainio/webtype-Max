e32bbdf7695fc599e80323c3d9666b36abd7c9f9
MaxMainio <beidler.max@gmail.com>
2022-04-07 15:40:55 -0400

    works

--- exercises/w7/js/Input-Range.js
+++ exercises/w7/js/Input-Range.js
@@ -1,40 +1,16 @@
 console.log("https://www.youtube.com/watch?v=NuAKnbIr6TE");
 
-// weight slider
+$(document).ready(function(){
 
-var wghtslider = document.getElementById("weight");
-var wghtoutput = document.getElementById("weight-value");
-var fontwght = document.querySelector(":root");
+    $('input[type=range]').on('input', function(){
+        let inputType = $(this).attr('id');
+        let inputValue = $(this).val();
 
-console.log(fontwght.style);
+        if (inputType == 'weight'){
+            $('#content').css('--text-weight', inputValue);
+        }else{
+            $('#content').css('--text-width', inputValue);
+        }
+    });
 
-wghtoutput.innerHTML = wghtslider.value;
-
-wghtslider.oninput = function() {
-    wghtoutput.innerHTML = this.value;
-}
-
-wghtslider.addEventListener("mousemove", function(){
-    var x = wghtslider.value;
-    var color = 'linear-gradient(90deg, var(--almost-red)' + (x -10)/(500-10) * 100 + '%, var(--almost-white)' + (x -10)/(500-10) * 100 + '%)';
-
-    wghtslider.style.background = color;
-})
-
-// width slider
-
-var wdthslider = document.getElementById("width");
-var wdthoutput = document.getElementById("width-value");
-
-wdthoutput.innerHTML = wdthslider.value;
-
-wdthslider.oninput = function() {
-    wdthoutput.innerHTML = this.value;
-}
-
-wdthslider.addEventListener("mousemove", function(){
-    var x = wdthslider.value;
-    var color = 'linear-gradient(90deg, var(--almost-red)' + x / 5 + '%, var(--almost-white)' + x / 5 + '%)';
-
-    wdthslider.style.background = color;
-})
+});
