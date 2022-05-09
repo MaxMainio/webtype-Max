6ffe90b84b7f08c9e48700b1de64a6593be9b983
MaxMainio <beidler.max@gmail.com>
2022-05-08 20:24:11 -0400

    vlogo final

--- extras/e1/js/vLogo-script.js
+++ extras/e1/js/vLogo-script.js
@@ -6,11 +6,25 @@ $(document).ready(function(){
         let inputType = $(this).attr('id');
         let inputValue = $(this).val();
 
-        if (inputType == 'weight'){
-            $('#content').css('--text-weight', inputValue);
-        }else{
-            $('#content').css('--text-width', inputValue);
-        }
+        if (inputType == 'cross-offset'){
+            $('#show-case').css('--cross-offset', inputValue);
+        }else{}
+
+        if (inputType == 'curvature'){
+            $('#show-case').css('--curvature', inputValue);
+        }else{}
+
+        if (inputType == 'rspine'){
+            $('#show-case').css('--rspine', inputValue);
+        }else{}
+
+        if (inputType == 'lspine'){
+            $('#show-case').css('--lspine', inputValue);
+        }else{}
+
+        if (inputType == 'thickness'){
+            $('#show-case').css('--thickness', inputValue);
+        }else{}
     });
 
 });
