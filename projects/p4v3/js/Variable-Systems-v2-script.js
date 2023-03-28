454fede09eaeb7f4942ac43878c3005f12112245
MaxMainio <beidler.max@gmail.com>
2023-03-28 10:43:33 -0400

    new generative on the way

---
+++ projects/p4v3/js/Variable-Systems-v2-script.js
@@ -0,0 +1,88 @@
+console.log("https://www.youtube.com/watch?v=NuAKnbIr6TE");
+
+let poemList = [];
+let poemCount = 0;
+
+
+$(document).ready(function(){
+
+  function getPoem(){
+    
+    $('header').addClass('loading');
+
+    $.getJSON("https://www.poemist.com/api/v1/randompoems")
+      .done(function( data ) {
+          console.log(data);
+          poemList = data; // get poem data
+          displayPoem(0);  // display first poem
+          $('header').removeClass('loading');
+        });
+  }  
+  
+  function displayPoem(poemIndex){
+    let poem = poemList[poemIndex];
+    let author = poem["poet"]; // object with name: nameofauthor, url: urlofauthor
+
+
+
+
+    let text = poem["content"];
+    var sentance = text.split(/\n/);
+    var words = [];
+
+    for (var i = 0; i < sentance.length; i++) {
+      var temp = sentance[i].split(/\s/);
+      words.push(temp);
+    }
+
+    poemWrite = "";
+
+    for (var i = 0; i < words.length; i++) {
+      poemWrite = poemWrite + "<select>";
+      for (var j = 0; j < words[i].length; j++) {
+        poemWrite = poemWrite + "<option>" + words[i][j] + "</option>";
+      }
+      poemWrite = poemWrite + "</select>";
+    }
+
+
+
+
+    $('#title').html(poem["title"] + ' ' + '<a href="'+author["url"]+'" target="_blank">'+author["name"]+'</a>');
+    $('#author').html('<a href="'+author["url"]+'" target="_blank">'+author["name"]+'</a>');
+
+    $('#poem').append(poemWrite);
+    $('#index').html(poemCount +1)
+  }
+
+  $("#append").click(function(){
+
+    if( !$('header').hasClass('loading') ){
+       // only display poem if JSON from API has fully loaded
+
+       if(poemCount < 4){        
+         poemCount ++; // increment poem index until 5
+         displayPoem(poemCount);
+       }else{
+         getPoem(); // get new batch of poems
+         poemCount = 0; // reset poem index
+       }       
+    }
+         
+  });
+  
+  getPoem();
+
+  $("#block").click(function(){
+
+    $('select').toggleClass('blockmode');
+
+    if( !$('select').hasClass('blockmode') ){
+      !$('select').removeClass('blockmode');
+    }else{
+      !$('select').addClass('blockmode');
+    }
+         
+  });
+
+});
